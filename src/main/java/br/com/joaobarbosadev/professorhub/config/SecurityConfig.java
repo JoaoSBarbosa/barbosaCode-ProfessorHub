30a485b610cd292f3f0a0a392906d740c1ff20e9
João <contato.jsbarbosa@gmail.com>
2025-02-05 21:41:15 -0300

    :wewnch: chore: Implementado inicialmente o SecurityConfig para controle e bloqueio de requisições

---
+++ src/main/java/br/com/joaobarbosadev/professorhub/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.com.joaobarbosadev.professorhub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // A partir do Spring Security 6, a configuração de segurança não é feita estendendo WebSecurityConfigurerAdapter.
+    // Agora, utilizamos beans do tipo SecurityFilterChain para configurar as regras de segurança.
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+
+}
