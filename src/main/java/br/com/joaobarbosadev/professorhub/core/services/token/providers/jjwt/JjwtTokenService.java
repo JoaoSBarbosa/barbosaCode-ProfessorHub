2df659b0f7f7d53df6aa9c6eca45fb1b7a9b97a7
João <contato.jsbarbosa@gmail.com>
2025-02-07 20:36:42 -0300

    wrench: chore: IMplementado classe para configuração da variaveis de autenticação referenciando properties.yaml

--- src/main/java/br/com/joaobarbosadev/professorhub/core/services/token/providers/jjwt/JjwtTokenService.java
+++ src/main/java/br/com/joaobarbosadev/professorhub/core/services/token/providers/jjwt/JjwtTokenService.java
@@ -1,8 +1,15 @@
 package br.com.joaobarbosadev.professorhub.core.services.token.providers.jjwt;
 
 import br.com.joaobarbosadev.professorhub.core.services.token.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class JjwtTokenService implements TokenService {
+
+    private final JjwtConfigProperties configProperties;
+
     @Override
     public String generateAcessToken(String subject) {
         return "";
