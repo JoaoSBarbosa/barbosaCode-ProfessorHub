dabcf37b8fc89f5e6b09ba11c7fd5b1f2fe5214f
João <contato.jsbarbosa@gmail.com>
2025-02-07 21:14:04 -0300

    wrench: chore: Implementação inicial de serviço para geração de tokens

--- src/main/java/br/com/joaobarbosadev/professorhub/core/services/token/providers/jjwt/JjwtTokenService.java
+++ src/main/java/br/com/joaobarbosadev/professorhub/core/services/token/providers/jjwt/JjwtTokenService.java
@@ -1,8 +1,17 @@
 package br.com.joaobarbosadev.professorhub.core.services.token.providers.jjwt;
 
 import br.com.joaobarbosadev.professorhub.core.services.token.TokenService;
+import br.com.joaobarbosadev.professorhub.core.services.token.exceptions.TokenServiceException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @Service
@@ -12,26 +21,60 @@ public class JjwtTokenService implements TokenService {
 
     @Override
     public String generateAcessToken(String subject) {
-        return "";
+        return generateToken(
+                subject,
+                configProperties.getAccessTokenExpirationInSeconds(),
+                configProperties.getAccessTokenSigningKey()
+        );
     }
 
     @Override
     public String getSubjectFromAcessToken(String token) {
-        return "";
+        return getClaims(token, configProperties.getAccessTokenSigningKey()).getSubject();
     }
 
     @Override
     public String generateRefreshToken(String subject) {
-        return "";
+        return generateToken(subject, configProperties.getRefreshTokenExpirationInSeconds(), configProperties.getRefreshTokenSigningKey());
     }
 
     @Override
     public String getSubjectFromRefreshToken(String refreshToken) {
-        return "";
+        return getClaims(refreshToken, configProperties.getRefreshTokenSigningKey()).getSubject();
     }
 
     @Override
-    public void validateAcessToken(String... token) {
+    public void inlidateAcessToken(String... token) {
+
+    }
+
+
+    private Claims getClaims(String token, String signingKey) {
+        try {
+            return tryGetClaims(token, signingKey);
+        } catch (JwtException e) {
+            throw new TokenServiceException(e.getLocalizedMessage());
+        }
+    }
+
+    private Claims tryGetClaims(String token, String signingKey) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(signingKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private String generateToken(String subject, Long expirationInSeconds, String signingKey) {
+        var today = Instant.now();
+        var expirationDate = today.plusSeconds(expirationInSeconds);
+        return Jwts.builder()
+                .setClaims(new HashMap<String, Object>())
+                .setSubject(subject)
+                .setIssuedAt(Date.from(today))
+                .setExpiration(Date.from(expirationDate))
+                .signWith(Keys.hmacShaKeyFor(signingKey.getBytes()))
+                .compact();
 
     }
 }
